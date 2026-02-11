a83392645b000d3cc38841d6a6c2d6142fd92aab
Davi Lima <davidlima35566@gmail.com>
2026-02-10 23:05:26 -0300

    Adção do menu principal

---
+++ src/main/java/Menu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static int MenuInicial(){
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("1. Cadastrar novo pet");
+        System.out.println("2. Alterar os dados do pet cadastrado");
+        System.out.println("3. Deletar um pet cadastrado");
+        System.out.println("4. Listar todos os pets cadastrados");
+        System.out.println("5. Listar pets por algum criterio (idade, nome, raça");
+        System.out.println("6. Sair");
+        System.out.print("-> ");
+        return leitura.nextInt();
+    }
+}
---
+++ src/main/java/SistemaDeCadastro.java
@@ -0,0 +1,24 @@
+public class SistemaDeCadastro {
+    public void main(){
+        while (true){
+            int opt;
+            opt = Menu.MenuInicial();
+
+            switch (opt){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+            }
+        }
+    }
+}
