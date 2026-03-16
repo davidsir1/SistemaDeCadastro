09b1159391975b7940021f2eeb496176c06206bf
Davi Lima <davidlima35566@gmail.com>
2026-03-16 11:52:05 -0300

    Refatoração da classe Menu na função cadastrarPet e adição dos cases das perguntas sobre o tipo do animal e o sexo

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,3 +1,6 @@
+import entities.Pet;
+import entities.PetSexo;
+import entities.PetTipo;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -7,7 +10,6 @@ import java.util.Scanner;
 public class Menu {
 
     static final String caminho_formulario = "data\\forms\\formulario.txt";
-    static final String campoEmBranco = "NÃO INFORMADO"; // Constante para ser utilizando em campos nulos
 
     public static int menuInicial() {
         Scanner leitura = new Scanner(System.in);
@@ -23,14 +25,13 @@ public class Menu {
         else return 0;
     }
 
-    public static Pet cadastrarPet() {
+    public static void cadastrarPet() {
         File arquivo_formulario = new File(caminho_formulario);
-        String dados[] = new String[7];
         Pet novoPet = new Pet();
 
         if (!arquivo_formulario.exists()) {
             System.out.println("O arquivo não existe!");
-            return null;
+            return;
         }
 
         try (FileReader fr = new FileReader(caminho_formulario)) {
@@ -45,45 +46,69 @@ public class Menu {
                 switch (perguntaAtual) {
                     case 0: // Pergunta sobre nome e sobrenome
                         System.out.print("-> ");
-                        dados[perguntaAtual] = leitura.nextLine();
+                        String nome_completo = leitura.nextLine();
 
                         // Verifica se o usuário não informou nome ou sobrenome ou
                         // Verifica se o nome e sobrenome possui caracter especial e apenas letras
-                        if (dados[perguntaAtual].isBlank()) {
-                            dados[perguntaAtual] = campoEmBranco;
-                        } else if (!dados[perguntaAtual].matches("^[A-Za-z ]+")) {
+                        if (nome_completo.isBlank()) {
+                            novoPet.setNome(Pet.NAO_INFORMADO);
+                        } else if (!nome_completo.matches("^[A-Za-z ]+")) {
                             System.out.println(
                                     "Apenas letras são permitidas e sem caracteres especiais."
                             );
-                            return null;
+                            return;
                         }
 
                         // Verificar se possui nome e sobrenome
-                        String[] partes = dados[perguntaAtual].split("\\s");
+                        String[] partes = nome_completo.split("\\s");
                         if (partes.length == 1) {
-                            System.out.println("Por favor, inclua nome e sobrenome do pet.");
-                            return null;
+                            System.out.println("Por gentileza, inclua nome e sobrenome do pet.");
+                            return;
                         }
+                        
+                        novoPet.setNome(nome_completo);
 
                         break;
+                    case 1: // Pergunta tipo do animal (Gato/Cachorro)
+                        System.out.println("1. Cachorro | 2. Gato");
+                        System.out.print("-> ");
+                        int tipo = leitura.nextInt();
+                        
+                        novoPet.setTipoAnimal(tipo == 1 ? PetTipo.CACHORRO : PetTipo.GATO);
+                        break;
+                    case 2: // Pergunta sobre o sexo do animal
+                        System.out.println("1. Macho | 2. Fêmea");
+                        System.out.print("-> ");
+                        int sexo = leitura.nextInt();
+                        
+                        novoPet.setSexo(sexo == 1 ? PetSexo.MACHO : PetSexo.FEMEA);
+                        
+                        leitura.nextLine(); // Limpa o buffer;
+                        break;
                     case 3: // Pergunta sobre o endereço
+                        String endereco;
                         System.out.print("i. Digite o número da casa: ");
                         String numeroCasa = leitura.nextLine();
 
                         if (!numeroCasa.isBlank()){
-                            if (numeroCasa.matches("^[0-9]+")){
-                                dados[perguntaAtual] = numeroCasa + ",";
+                            if (!numeroCasa.matches("^[0-9]+")){
+                                System.out.println("Digite apenas números positivos.");
+                                return;
+                            } else {
+                                endereco = numeroCasa + ",";
                             }
                         } else {
-                            dados[perguntaAtual] = campoEmBranco + ",";
+                            endereco = Pet.NAO_INFORMADO + ",";
                         }
 
                         System.out.print("ii. Digite a cidade: ");
-                        dados[perguntaAtual] += leitura.nextLine() + ",";
+                        endereco += leitura.nextLine() + ",";
 
                         System.out.print("iii. Digite a rua: ");
-                        dados[perguntaAtual] += leitura.nextLine();
-
+                        endereco += leitura.nextLine();
+                        
+                        novoPet.setEndereco(endereco);
+                        
                         break;
                     case 4: // Pergunta sobre idade
                         System.out.print("-> ");
@@ -91,13 +116,13 @@ public class Menu {
 
                         if (!idade.isBlank()){
                             if (idade.matches("^[0-9]+") && Integer.parseInt(idade) < 21){
-                                dados[perguntaAtual] = idade;
+                                novoPet.setIdade(idade);
                             } else {
                                 System.out.println("Valor inválido. Digite apenas números ou valores menor ou igual a 20.");
-                                return null;
+                                return;
                             }
                         } else {
-                            dados[perguntaAtual] = campoEmBranco;
+                           novoPet.setIdade(Pet.NAO_INFORMADO);
                         }
 
                         break;
@@ -108,35 +133,35 @@ public class Menu {
                         if (!peso.isBlank()){
                             if (peso.matches("^[0-9]*\\.?[0-9]+") &&
                                     (Double.parseDouble(peso) < 60.0 && Double.parseDouble(peso) > 0.5)){
-                                dados[perguntaAtual] = peso.replace(',', '.');
+                                novoPet.setPeso(peso.replace(',', '.'));
                             } else {
                                 System.out.println("Valor inválido. Digite apenas números ou valores entre 0.5 e 60 kg.");
-                                return null;
+                                return;
                             }
                         } else {
-                            dados[perguntaAtual] = campoEmBranco;
+                            novoPet.setPeso(Pet.NAO_INFORMADO);
                         }
 
                         break;
                     case 6: // Pergunta sobre raça
                         System.out.print("-> ");
-                        dados[perguntaAtual] = leitura.nextLine();
+                        String raca = leitura.nextLine();
 
-                        if (!dados[perguntaAtual].isBlank()){
-                            if (!dados[perguntaAtual].matches("^[A-Za-z]+")) {
+                        if (!raca.isBlank()){
+                            if (raca.matches("^[A-Za-z]+")) {
+                                novoPet.setRaca(raca);
+                            } else {
                                 System.out.println(
                                         "Apenas letras são permitidas e sem caracteres especiais."
                                 );
-                                return null;
+                                return;
                             }
                         } else {
-                            dados[perguntaAtual] = campoEmBranco;
+                            novoPet.setRaca(Pet.NAO_INFORMADO);
                         }
 
                         break;
                     default:
-                        System.out.print("-> ");
-                        dados[perguntaAtual] = leitura.nextLine();
                         break;
                 }
 
@@ -147,12 +172,8 @@ public class Menu {
             e.printStackTrace();
         }
 
-        novoPet.salvarPet(dados);
-
         novoPet.exibirDados();
 
         novoPet.salvarArquivoPet();
-
-        return novoPet;
     }
 }
